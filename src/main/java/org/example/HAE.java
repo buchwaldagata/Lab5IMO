17e2911a2099121b16556bd9e634f9450570a7bc
buchwaldagata <buchwaldagata@gmail.com>
2024-05-24 08:47:47 +0200

    tworzenie listy zawierajacej te same krawedzie

--- src/main/java/org/example/HAE.java
+++ src/main/java/org/example/HAE.java
@@ -1,8 +1,9 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
 
 class HAE {
 
@@ -29,6 +30,78 @@ class HAE {
             }
             map.put(key, cycles_X);
         }
+
+        //wylosowanie rodziców
+        Random random = new Random();
+        Double firstParentKey = 0.0;
+        List<List<Integer>> firstParentValue = null;
+        Double secondParentKey = 0.0;
+        List<List<Integer>> secondParentValue = null;
+        while(true){
+            int randomNumber = random.nextInt(20);
+            int secondRandomNumber = random.nextInt(20);
+            int number = 0;
+            if (randomNumber != secondRandomNumber){
+                for (Map.Entry<Double, List<List<Integer>>>i: map.entrySet()) {
+                    if (number == randomNumber){
+                        firstParentKey = i.getKey();
+                        firstParentValue = i.getValue();
+                    }
+                    if (number == secondRandomNumber){
+                        secondParentKey = i.getKey();
+                        secondParentValue = i.getValue();
+                    }
+                    number++;
+                }
+                break;
+            }
+        }
+
+        Map<Integer, List<Integer[]>> equals = new HashMap<>();
+        int numberDictionary = 0;
+        for(int i = 0; i < firstParentValue.get(0).size(); i++){
+                for(int k = 0; k < secondParentValue.get(0).size(); k++){
+                       Integer firstVertexFirstParent = firstParentValue.get(0).get(i);
+                       Integer firstVertexFirstParentSecondCycle = firstParentValue.get(1).get(i);
+                       Integer secondVertexFirstParent = firstParentValue.get(0).get(i+1);
+                       Integer secondVertexFirstParentSecondCycle = firstParentValue.get(1).get(i+1);
+                       Integer firstVertexSecondParent = secondParentValue.get(0).get(k);
+                       Integer firstVertexSecondParentSecondCycle = secondParentValue.get(1).get(k);
+                       Integer secondVertexSecondParent = secondParentValue.get(0).get(k+1);
+                       Integer secondVertexSecondParentSecondCycle = secondParentValue.get(1).get(k+1);
+
+
+                       List<Integer[]> list = new ArrayList<>();
+
+                       if (firstVertexFirstParent == firstVertexSecondParent && secondVertexFirstParent == secondVertexSecondParent){
+                           list.add(new Integer[]{firstVertexFirstParent, secondVertexFirstParent});
+                           equals.put(numberDictionary, list);
+                           numberDictionary++;
+                       }
+                       else if (firstVertexFirstParentSecondCycle == firstVertexSecondParentSecondCycle && secondVertexFirstParentSecondCycle == secondVertexSecondParentSecondCycle) {
+                           list.add(new Integer[]{firstVertexFirstParentSecondCycle, secondVertexFirstParentSecondCycle});
+                           equals.put(numberDictionary, list);
+                           numberDictionary++;
+                       }
+                       else if (firstVertexFirstParent == firstVertexSecondParentSecondCycle && secondVertexFirstParent == secondVertexSecondParentSecondCycle){
+                           list.add(new Integer[]{firstVertexFirstParent, secondVertexFirstParent});
+                           equals.put(numberDictionary, list);
+                           numberDictionary++;
+                       }
+                       else if (firstVertexFirstParentSecondCycle == firstVertexSecondParent && secondVertexFirstParentSecondCycle == secondVertexSecondParent){
+                            list.add(new Integer[]{firstVertexFirstParentSecondCycle, secondVertexFirstParentSecondCycle});
+                            equals.put(numberDictionary, list);
+                            numberDictionary++;
+                       }
+
+
+                }
+
+        }
+
+
+        
+
         System.out.println("SKONCZONE");
     }
 
@@ -40,4 +113,20 @@ class HAE {
         length += distanceMatrix.get(solution.get(solution.size() - 1)).get(solution.get(0));
         return length;
     }
+
+//    public void solutionToCsv(String path) throws IOException {
+//        FileWriter fileWriter = new FileWriter(path);
+//        PrintWriter printWriter = new PrintWriter(fileWriter);
+//        printWriter.print("cycle,x,y\n");
+//        for (Integer a : cycles_X.get(0)) {
+//            printWriter.printf("%s,%d,%d\n","a", instance.coordinates.get(a).getKey(), instance.coordinates.get(a).getValue());
+//        }
+//        printWriter.printf("%s,%d,%d\n","a", instance.coordinates.get(cycles_X.get(0).get(0)).getKey(), instance.coordinates.get(cycles_X.get(0).get(0)).getValue());
+//        for (Integer a : cycles_X.get(1)) {
+//            printWriter.printf("%s,%d,%d\n","b", instance.coordinates.get(a).getKey(), instance.coordinates.get(a).getValue());
+//        }
+//        printWriter.printf("%s,%d,%d\n","b", instance.coordinates.get(cycles_X.get(1).get(0)).getKey(), instance.coordinates.get(cycles_X.get(1).get(0)).getValue());
+//        printWriter.close();
+//    }
+
 }
